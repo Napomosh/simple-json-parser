618a38e111759ac57402bfb2d921dd504b0f905a
Alexander <sashulik999@gmail.com>
2021-02-24 11:03:05 +0300

    Add arguments in main()

--- test_parser.cpp
+++ test_parser.cpp
@@ -2,13 +2,19 @@
 
 #include "parser.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        std::cout << "Incorrect input" << std::endl;
+        return 1;
+    }
+    else if (argc > 2) {
+        std::cout << "Only the first file will be parse" << std::endl;
+    }
     setlocale(LC_ALL, "Russian");
 
-    std::string file_name = "";
-    std::cout << "Enter file name with JSON information: ";
-    std::cin >> file_name;
+    std::string file_name = argv[1];
+    std::cout << "Your file for parsing" << file_name << std::endl << std::endl;
 
     parser p(file_name);
     p.parse();
